da73f5f72856aa2bd94a25d236ed8cba984144ce
mdmathewdc <mathewdony007@gmail.com>
2021-09-26 17:25:23 +1000

    Created the variables

---
+++ index.js
@@ -0,0 +1,6 @@
+// console.log("Testing");
+
+const readline = require('readline');
+
+var robot = { "x": 0, "y": 0, "f": "north", "placed": false }
+var directions = ["north", "east", "south", "west"]
